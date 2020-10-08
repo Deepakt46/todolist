fd51e5e911babed3ae47d17b6ab04dcdbe8b15c4
Deepakt46 <deepakkoomully10@gmail.com>
2020-10-08 11:25:51 +0530

    Deletion Working

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const Todo = ({text}) =>{
+const Todo = ({text,todos, setTodo,todo}) =>{
+  const Deleting = (e) =>{
+    setTodo(todos.filter((list)=> list.id !== todo.id ))
+
+}
     return(
         <div>
            <li>
              {text}
+             <button onClick={Deleting} className='f9 w-09'>Delete</button>
+             
            </li>
         </div>
 
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -3,21 +3,19 @@ import Todo from './Todo'
 
 const TodoList = ({setInput, inputText, todos, setTodo}) =>{
     const inputHandler = (e) =>{
-        console.log(e.target.value)
+     
         setInput(e.target.value)
     }
-    const Save = (e) =>{
-        e.preventDefault()
+    const Save = (event) =>{
+        event.preventDefault()
+        if(inputText!==''){
         setTodo([
-            ...todos,{ text:inputText , id:Math.random() *1000},
+            ...todos,{ text:inputText, id:Math.random() *1000 },
         ])
         setInput('')
-    }
+    }}
     
-    const Deleting = (e) =>{
-        setTodo(todos.filter((list)=> list.id !== todos.id ))
-
-    }
+  
    
 
     return(
@@ -27,15 +25,17 @@ const TodoList = ({setInput, inputText, todos, setTodo}) =>{
          {todos.map( (todo)=>(
              <Todo 
              text={todo.text}
-             key={todos.id}
+             key={todo.id}
+             todos={todos} setTodo={setTodo}
+             todo={todo}
              />
          ))}
            </ul>
             <div>
-                <input value={inputText} onChange={inputHandler} className='f4 w-20 center pa1' type='text' placeholder='add items'></input>
+                <input  value={inputText} onChange={inputHandler} className='f4 w-20 center pa1' type='text' placeholder='add items'></input>
                <div>
                 <button onClick={Save} className='f9 w-09'>Save</button>
-                <button onClick={Deleting} className='f9 w-09'>Delete</button>
+                
                 </div>
             </div>
         </div>
